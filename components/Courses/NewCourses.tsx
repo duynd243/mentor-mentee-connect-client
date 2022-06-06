fdb41b9204eb84dce46b08c68a8363f6ea4a0aca
duyndse150396 <duyndse@gmail.com>
2022-06-06 15:44:40 +0700

    deploy

--- components/Courses/NewCourses.tsx
+++ components/Courses/NewCourses.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-const NewCourses = ({ allCourseItems }) => {
+const NewCourses = ({ allCourseItems }:any) => {
   return (
     <>
       <div className="course__sidebar-widget white-bg">
         <div className="course__sidebar-course">
           <h3 className="course__sidebar-title">New courses</h3>
           <ul>
-            {allCourseItems?.slice(7, 10).map((course, index) => {
+            {allCourseItems?.slice(7, 10).map((course:any, index:any) => {
               return (
                 <li key={index}>
                   <div className="course__sm d-flex align-items-center mb-30">
