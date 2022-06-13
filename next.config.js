00ac0b6b5b39678b9a6ce11ec22fde6262c31d75
duyndse150396 <duyndse@gmail.com>
2022-06-13 08:27:57 +0700

    u

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     baseUrl: "https://mentor-mentee-connect-api.tk/api/v1",
     sampleUrl: "https://obscure-shelf-38503.herokuapp.com",
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
